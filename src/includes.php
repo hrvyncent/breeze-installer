2062fe18a900cec715911e8d0faa84122e7c75f2
harvicapino22 <harvicapino22@gmail.com>
2023-06-25 12:19:59 +0800

    created breeze-installer

---
+++ src/includes.php
@@ -0,0 +1,30 @@
+<?php
+
+$includes = [
+    'composer.json',
+    'composer.lock',
+    'package.json',
+    'package-lock.json',
+    'postcss.config.js',
+    'tailwind.config.js',
+    'tsconfig.json',
+    'vite.config.js',
+    'app/Http/Kernel.php',
+    'app/Http/Middleware/HandleInertiaRequests.php',
+    'app/Providers/RouteServiceProvider.php',
+    'resources/css/app.css',
+    'resources/js/app.js',
+    'resources/js/app.ts',
+    'resources/js/bootstrap.js',
+    'resources/js/bootstrap.ts',
+    'resources/js/Components/ApplicationLogo.vue',
+    'resources/js/types',
+    'resources/views/app.blade.php',
+    'routes/auth.php',
+    'routes/web.php',
+    'tests/Pest.php',
+    'tests/Feature/ExampleTest.php',
+    'tests/Unit/ExampleTest.php',
+];
+
+return $includes;
